df2ea58e4d4ecd187250c8e6316d0ab7a208ce98
RicardoP-19 <ricardo-pinto@web.>
2024-08-10 17:56:25 +0200

    end-projekt

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let questions = [
   {
-    "question": "Wer hat HTML erfunden",
+    "question": "Wer hat HTML erfunden?",
     "answer_1": "Robbie Wlliams",
     "answer_2": "Lady Gaga",
     "answer_3": "Tim Berner-Lee",
@@ -8,7 +8,7 @@ let questions = [
     "right_answer": 3
   },
   {
-    "question": "Was beduetet das HTML Tag &lt;a&gt;?",
+    "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
     "answer_1": "Text Fett",
     "answer_2": "Container",
     "answer_3": "Ein Link",
@@ -16,7 +16,7 @@ let questions = [
     "right_answer": 3
   },  
   {
-    "question": "Wie bindet man eine Website in eine Website ein?",
+    "question": "Wie bindet man eine Website in einer Website ein?",
     "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
     "answer_2": "&lt;iframe&gt",
     "answer_3": "&lt;frame&gt",
@@ -42,8 +42,78 @@ let questions = [
 ];
 
 
+let rightQuestions = 0;
+let currentQuestion = 0;
+let AUDIO_SUCCESS = new Audio('sound/success.mp3');
+let AUDIO_FAIL = new Audio('sound/fail.mp3');
+
 function init() {
-  let length = questions.length;
-  let status = document.getElementById('current-status');
-  status.innerHTML = /*html*/`<b>1</b> von <b>${length}</b> Fragen`;
+  document.getElementById('all-questions').innerHTML = questions.length;
+  showQuestion();
+}
+
+
+function showQuestion() {
+  if (currentQuestion >= questions.length) {
+    // Show End Screen
+    document.getElementById('end-screen').style = '';   
+    document.getElementById('question-body').style = 'display: none';
+    document.getElementById('amount-of-questions').innerHTML = questions.length;
+    document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+  }else {
+    // Show question
+    let question = questions[currentQuestion];
+    document.getElementById('question-number').innerHTML = currentQuestion + 1;
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+  }
+}
+
+
+function answer(selection) {
+  let question = questions[currentQuestion];
+  let selectedQuestionNumber  = selection.slice(-1);
+  let idOfRightAnswer = `answer_${question['right_answer']}`;
+  if (selectedQuestionNumber == question['right_answer']) {
+    document.getElementById(selection).parentNode.classList.add('bg-success');
+    AUDIO_SUCCESS.play();
+    rightQuestions++;
+  } else {    
+    document.getElementById(selection).parentNode.classList.add('bg-danger');
+    document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    AUDIO_FAIL.play();
+  }
+  document.getElementById('next-button').disabled = false;
+}
+
+
+function nextQuestion() {
+  currentQuestion++;
+  document.getElementById('next-button').disabled = true;
+  resetAnswerButtons();
+  showQuestion();
+}
+
+
+function resetAnswerButtons() {
+  document.getElementById('answer_1').parentNode.classList.remove('bg-danger')  
+  document.getElementById('answer_1').parentNode.classList.remove('bg-success')
+  document.getElementById('answer_2').parentNode.classList.remove('bg-danger')
+  document.getElementById('answer_2').parentNode.classList.remove('bg-success')
+  document.getElementById('answer_3').parentNode.classList.remove('bg-danger')
+  document.getElementById('answer_3').parentNode.classList.remove('bg-success')
+  document.getElementById('answer_4').parentNode.classList.remove('bg-danger')
+  document.getElementById('answer_4').parentNode.classList.remove('bg-success')
+}
+
+
+function restartGame() {
+  document.getElementById('end-screen').style = 'display: none';   
+  document.getElementById('question-body').style = '';
+  rightQuestions = 0;
+  currentQuestion = 0;
+  init();
 }
